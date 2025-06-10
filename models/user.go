ff67f9fd9e4549a81d658209372bca1e71985a50
Archit Batra <dev@architbatra.in>
2025-06-10 13:34:57 +0530

    hashing passwords

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/archit-batra/event-booking-rest-api/db"
+import (
+	"github.com/archit-batra/event-booking-rest-api/db"
+	"github.com/archit-batra/event-booking-rest-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
